88f4d9974fd4d4ec355d5ed070e0abbd5c3842a1
Nohbin2 <bean__2@naver.com>
2025-03-28 13:32:40 +0900

    Spring Start

---
+++ src/main/java/cju/parkinggo/ParkinggoApplication.java
@@ -0,0 +1,13 @@
+package cju.parkinggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinggoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkinggoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cju/parkinggo/ParkinggoApplicationTests.java
@@ -0,0 +1,13 @@
+package cju.parkinggo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinggoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
